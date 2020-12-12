afe71044adc14f8e7c61fe07b53aa2981e6826b5
Francesco Bianco <francescobianco@opencrmitalia.com>
2020-12-12 16:41:04 +0100

    new release

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# mysql
+# Docker CI
+
+> The Premium Docker Image for CI/CD
--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 versions=(
-  5.7.28
+  19.03.8
 )
 
 for version in "${versions[@]}"; do
   mkdir -p ${version}
   sed -e 's!%{version}!'"${version}"'!' Dockerfile.template > ${version}/Dockerfile
-  cp dataset.sh ${version}/dataset.sh
-  chmod +x ${version}/dataset.sh
-  docker build -t javanile/mysql:${version} ${version}
-  docker push javanile/mysql:${version}
+  cp docker-compose ${version}/docker-compose
+  chmod +x ${version}/docker-compose
+  docker build -t javanile/docker-ci:${version} ${version}
+  docker push javanile/docker-ci:${version}
 done
 
 git add .
