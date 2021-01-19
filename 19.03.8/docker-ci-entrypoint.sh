9093c9eaf3a5fadfab797e33122e0cfb5742af77
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-01-19 02:32:14 +0100

    new release

--- 19.03.8/docker-ci-entrypoint.sh
+++ 19.03.8/docker-ci-entrypoint.sh
@@ -8,6 +8,8 @@ printenv
 git config --global user.name "Docker CI"
 git config --global user.email "${GITLAB_USER_EMAIL}"
 
+[[ -n "${GITLAB_DEPLOY_KEY}" ]] && GITLAB_DEPLOY_KEY=${CI_PROJECT_DIR}/${GITLAB_DEPLOY_KEY}
+
 if [[ -f "${GITLAB_DEPLOY_KEY}" ]]; then
     echo "Found deploy key: ${GITLAB_DEPLOY_KEY}"
     cp ${GITLAB_DEPLOY_KEY} /root/.ssh/deploy_key
